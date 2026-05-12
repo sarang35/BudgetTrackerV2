82c998fe575b6b68eef39b3a17a1d8f5c9e34bf2
sarang35 <sarangkrishnams@gmail.com>
2026-05-12 23:14:46 +0530

    Done a little frontend work and some error is remaining time-23:14

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import bigImage from "./assets/jakub-zerdzicki-U4-I4oH4xlg-unsplash.jpg";
+import Topic from "./components/LoginPage/Topic";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isSign, setIsSign] = useState("SignIn");
   return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+    <div
+      className="min-h-screen lg:bg- bg-cover bg-center flex justify-center items-center"
+      style={{ backgroundImage: `url(${bigImage})` }}
+    >
+      <div className="min-h-[500px] w-[300px] lg:h-[500px] flex items-center lg:w-[700px] rounded-2xl opacity-86 bg-white">
+        <div className="w-[50%] bg-amber-700 h-[90%] " >
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/LoginPage/Topic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Topic = () => {
+  return (
+    <div className='w-full lg:block rounded-2xl min-h-[100px] mt-5 lg:mt-14 flex justify-center items-center text-black text-[25px] lg:text-3xl font-bold '>
+        hello
+    </div>
+  )
+}
+
+export default Topic
